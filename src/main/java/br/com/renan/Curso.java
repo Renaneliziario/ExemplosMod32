5349840ed3f811f4a4fdec0810dd98e087aa2bd3
Renaneliziario <Renanqueiroz92@gmail.com>
2025-10-28 18:20:26 -0300

    adicionado classe curso

---
+++ src/main/java/br/com/renan/Curso.java
@@ -0,0 +1,49 @@
+package br.com.renan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "TB_CURSO")
+public class Curso {
+	
+	@Id
+	@GeneratedValue
+	@SequenceGenerator
+	private Long id;
+	private String codigo;
+	private String nome;
+	private String descricao;	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+
+}
